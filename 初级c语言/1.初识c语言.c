bfa7d7f5da054cfcc3731c1046b7ae36e86c4237
layue7 <2802767006@qq.com>
2023-11-28 11:01:38 +0800

    初级练习/笔记

---
+++ 初级c语言/1.初识c语言.c
@@ -0,0 +1,19 @@
+//初始c语言
+/*
+std - 标准
+i - input
+o - output 
+*/
+ 
+#include <stdio.h>
+//关于标准输入输出的文件 
+	
+//标准的主函数的写法，有且仅有一个 
+int a=0;
+int main()
+{
+	printf("hello\nword"); 
+	return 0;
+}
+
+
